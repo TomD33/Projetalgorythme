0c8d9588853a58061f886185ce41df2c27325d7a
Dauliac <Juliendauliac@gmail.com>
2016-11-16 16:57:27 +0100

    test pacman 1
    premier test pour le jeux pacman 1

---
+++ pacman/class.py
@@ -0,0 +1,41 @@
+class Movable:
+    """Classe définissant les entitées capables de se déplacer carractérisée par:
+        -sa position x
+        -sa position y
+        """
+
+    def __init__(self):
+        """Par défaut """"
+
+    def __deplacement__(test,x,test) :
+
+      if ypos == 1 or ypos == 10 or xpos == 10 xpos == 1 :
+          mov = 10
+      else mov = 1
+      if deplacement==up) and (chaine[x-12]==test) :
+          dep="y+"
+          rep=true
+      if (deplacement==down) and (chaine[x+12]==test) :
+          dep="y-"
+          rep=true
+      if (deplacement==left) and (chaine[x-1]==test)
+          dep="x+"
+          rep=true
+      if (deplacement==right) and (chaine[x+1]==test) :
+          dep="x-"
+          rep=true
+      else:
+          rep=false
+
+
+
+class Gohst(Movable):
+    def __init__(self):
+        "Par défaut les phantomes n'ont peur de rien"
+
+
+class PacMan(Movable):
+    def __init__(self):
+        "Par défaut pacman à peur des phantomes"
+        x =
+        fear = 'n'
---
+++ pacman/test.py
@@ -0,0 +1,29 @@
+# -*-coding:Utf-8 -*
+import os # On importe le module os
+
+
+
+print("Bonjour le monde !")
+chaine0 = "x"*22;
+chaine1 = "x"+20*" "+"x"
+chaine2 = chaine1
+chaine3 = chaine1
+chaine4 = chaine1
+chaine5 = chaine1
+chaine6 = chaine1
+chaine7 = chaine1
+chaine8 = chaine1
+chaine9 = chaine1
+chaine10 = chaine1
+print(chaine0)
+print(chaine1)
+print(chaine2)
+print(chaine3)
+print(chaine4)
+print(chaine5)
+print(chaine6)
+print(chaine7)
+print(chaine8)
+print(chaine9)
+print(chaine10)
+print(chaine0)
