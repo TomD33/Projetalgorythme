e17394a0f30e9f60af8a9aad5accfb1240aca98a
Dauliac <Juliendauliac@gmail.com>
2016-11-20 15:09:21 +0100

    labyrinthe fonctionnel
    first commit

---
+++ pacman/pacman.py
@@ -0,0 +1,54 @@
+
+M = ["X",0]
+V = ["  ",0]
+R = ["o ",1]
+P = ["G ",0]
+F = ["F ",0]
+Fpos = 30
+Ppos = 80
+
+Map = [R, X, X,
+ R, P, V,
+  X, X, X]
+
+#Map = [Case]*264
+#for i in range(264)
+def Afficher_Map():
+    for i in range(9):
+    print(Map[i][0], end='')
+    if i%3 == 2:
+        print()
+
+def Remplissage_Map():
+    for i in range(264)
+        if i <= 22: #Première ligne
+            Map[i] = M
+        if i%22 == 1 or i%22 == 0:  #Murs latérales
+            Map[i] = M
+        if n >= 242 and n >= 264:   #Derniere ligne
+            Map[n]= M
+def Mouvement(Moving_Pos):
+    if Tableau[Moving_Pos][0]+1 = " " or Tableau[Moving_Pos][0]-1 = " " #coté murs
+        mov = 1
+    if Tableau[Moving_Pos][0]+11 = " " or Tableau[Moving_Pos][0]-11 = " " #haut bas murs
+        mov = 1
+    if Tableau[Moving_Pos][0]+
+    else mov = 1 #déplacement normal
+    if deplacement==up) and (colones[pos+mov]==test) :
+        dep="y+"
+        rep=true
+    if (deplacement==down) and (colones[pos-mov]==test) :
+        dep="y-"
+        rep=true
+    if (deplacement==left) and (colones[pos-mov]==test)
+        dep="x+"
+        rep=true
+    if (deplacement==right) and (colones[pos+mov]==test) :
+        dep="x-"
+        rep=true
+    else:
+        rep=false
+
+
+
+While #Fonction pour trouver un carractère
